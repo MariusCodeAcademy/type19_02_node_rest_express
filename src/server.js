5c233573ae5e1d09fb354b0a151f30da5269789d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-14 10:58:38 +0200

    users api

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = 3000;
 
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+
 const users = [
   {
     id: 1,
@@ -51,6 +55,34 @@ const users = [
     },
   },
 ];
+
+// gauti visus userius
+// GET - /api/users - grazina visus vartotojus
+app.get('/api/users', (request, response) => {
+  // response.send('gauti visus userius');
+  response.json(users);
+});
+// gauti userius kuriu amzius dauiau nei 21
+app.get('/api/users/adults', (request, response) => {
+  // atfliltruoti users
+  const filtered = '';
+  // issiusti atfiltruotus atgal su response
+  response.send('gauti userius kuriu amzius dauiau nei 21');
+});
+// GET - /api/users/adults -> gauti userius kuriu amzius dauiau nei 21
+// gauti viena useri
+// GET - /api/users/single?userId=1 -> gauti viena useri
+app.get('/api/users/single', (request, response) => {
+  console.log('request.query ===', request.query);
+  const userId = 2;
+  // atfliltruoti users
+  const found = '';
+  // issiusti konkretu vartotoja atgal su response
+  // extra jei neradom pranesti kad nerasta
+  response.send('gauti viena useri');
+});
+// isrikiuoti userius pagal amziu
+// GET - /api/users/sort/age -> isrikiuoti userius pagal amziu
 
 app.listen(PORT, () => {
   console.log(`Users sever listening on port http://localhost:${PORT}`);
