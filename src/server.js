6734764c724963fe756a7e00742fa040a17215df
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-14 11:55:28 +0200

    users

--- src/server.js
+++ src/server.js
@@ -65,24 +65,52 @@ app.get('/api/users', (request, response) => {
 // gauti userius kuriu amzius dauiau nei 21
 app.get('/api/users/adults', (request, response) => {
   // atfliltruoti users
-  const filtered = '';
+  const filtered = users.filter((userObj) => userObj.age >= 25);
   // issiusti atfiltruotus atgal su response
-  response.send('gauti userius kuriu amzius dauiau nei 21');
+  // response.send('gauti userius kuriu amzius dauiau nei 21');
+
+  // ar radau nors viena useri?
+  if (filtered.length === 0) {
+    response.status(404).json({
+      success: false,
+      msg: 'No users in that age found',
+    });
+    return;
+  }
+
+  response.json({ success: true, data: filtered });
 });
 // GET - /api/users/adults -> gauti userius kuriu amzius dauiau nei 21
 // gauti viena useri
 // GET - /api/users/single?userId=1 -> gauti viena useri
 app.get('/api/users/single', (request, response) => {
   console.log('request.query ===', request.query);
-  const userId = 2;
+  const { userId } = request.query; // visada stringas
   // atfliltruoti users
-  const found = '';
   // issiusti konkretu vartotoja atgal su response
+  const found = users.find((uObj) => uObj.id === +userId);
+  console.log('found ===', found);
   // extra jei neradom pranesti kad nerasta
-  response.send('gauti viena useri');
+  if (!found) {
+    response.status(404).json({ msg: 'user not found' });
+    return;
+  }
+  // response.send('gauti viena useri');
+  response.json(found);
 });
 // isrikiuoti userius pagal amziu
 // GET - /api/users/sort/age -> isrikiuoti userius pagal amziu
+app.get('/api/users/sort/age', (request, response) => {
+  // isrikiuoti users
+  const usersCopy = sorted.slice();
+  // const usersCopy = [...users]
+  const sorted = usersCopy.sort((a, b) => a.age - b.age);
+  // issiusti atgal su response
+  // response.send('isrikiuoti userius pagal amziu');
+  response.json(sorted);
+});
+
+// GET - /api/users/city/la -> grazina vartotojus kuriu miestas 'Los Angeles'
 
 app.listen(PORT, () => {
   console.log(`Users sever listening on port http://localhost:${PORT}`);
